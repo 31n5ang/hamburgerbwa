44d1b2ce318673c95c097339d5ad8c0c6944e96a
31n5ang <31n5ang@gmail.com>
2024-08-01 22:38:40 +0900

    feat: 로그인 뷰 추가

--- src/main/java/kr/hamburgersee/domain/session/SessionConstants.java
+++
@@ -1,8 +0,0 @@
-package kr.hamburgersee.domain.session;
-
-public final class SessionConstants {
-    private SessionConstants() {}
-
-    public static final String MEMBER_SESSION_INFO = "memberSessionInfo";
-    public static final String REQUEST_REDIRECT_URI = "requestRedirectUri";
-}
