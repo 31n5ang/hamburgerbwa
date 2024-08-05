5eb255e1dc495f1bae9c2c384510f5a9f3d54cec
31n5ang <31n5ang@gmail.com>
2024-08-05 22:28:55 +0900

    feat: 페이징 조회 쿼리 추가

--- src/main/java/kr/hamburgersee/domain/review/ReviewRepository.java
+++ src/main/java/kr/hamburgersee/domain/review/ReviewRepository.java
@@ -1,10 +1,11 @@
 package kr.hamburgersee.domain.review;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -12,4 +13,13 @@ import java.util.Optional;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("select r from Review r join fetch r.member m where r.id = :reviewId")
     Optional<Review> findByIdWithMember(@Param("reviewId") Long reviewId);
+
+    @Query(
+            value = "select r " +
+                    "from Review r " +
+                    "left join fetch r.thumbnailImage ti " +
+                    "join fetch r.member m ",
+            countQuery = "select r from Review r"
+    )
+    Slice<Review> findSliceWithRelated(Pageable pageable);
 }
