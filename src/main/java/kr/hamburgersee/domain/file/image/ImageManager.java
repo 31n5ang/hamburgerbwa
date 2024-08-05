79eef5375ba0f54a2e2334768e09b9af9877b4b3
31n5ang <31n5ang@gmail.com>
2024-08-05 22:27:26 +0900

    feat: 이미지 파일 관리 기능 별도 분리

--- src/main/java/kr/hamburgersee/domain/file/image/ImageManager.java
+++ src/main/java/kr/hamburgersee/domain/file/image/ImageManager.java
@@ -1,11 +1,9 @@
 package kr.hamburgersee.domain.file.image;
 
 import kr.hamburgersee.domain.aws.storage.StorageUploadFileType;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface ImageManager {
-    public String uploadImage(byte[] file, String originalFilename, StorageUploadFileType storageUploadFileType);
+    String uploadImage(byte[] file, String originalFilename, StorageUploadFileType storageUploadFileType);
 
-    public void deleteImage(String url, StorageUploadFileType storageUploadFileType);
+    void deleteImage(String url, StorageUploadFileType storageUploadFileType);
 }
--- src/main/java/kr/hamburgersee/domain/file/image/S3StorageImageManager.java
+++ src/main/java/kr/hamburgersee/domain/file/image/S3StorageImageManager.java
@@ -4,7 +4,9 @@ import kr.hamburgersee.domain.aws.storage.StorageService;
 import kr.hamburgersee.domain.aws.storage.StorageUploadFileType;
 import kr.hamburgersee.domain.file.FileUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 public class S3StorageImageManager implements ImageManager {
     private final StorageService storageService;
