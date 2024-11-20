43ecdb5af044a25dcd2ec43357d4637b0682d3c5
31n5ang <31n5ang@gmail.com>
2024-11-21 02:47:52 +0900

    feat: redis session 정보 직렬화 방식 등록

--- src/main/java/kr/hamburgersee/domain/session/MemberSessionInfo.java
+++ src/main/java/kr/hamburgersee/domain/session/MemberSessionInfo.java
@@ -1,13 +1,16 @@
 package kr.hamburgersee.domain.session;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberSessionInfo implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
